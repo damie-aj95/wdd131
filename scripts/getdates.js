2ec2773b3afdeb9c2736324ac1d2cb7778a084b7
Damilare Ajayi <damie.aj95@gmail.com>
2026-07-05 01:39:03 +0100

    Move files to root and complete WDD131 assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// current year
+const currentYear = new Date();
+document.getElementById("currentyear").innerHTML = currentYear.getFullYear();
+
+// lastmodified
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
